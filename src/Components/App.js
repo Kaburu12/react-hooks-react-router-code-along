31a806f4b7a2aab7cc7d23a01e80e4f19ba5f175
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-07-19 16:56:45 +0300

    done

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,10 +7,11 @@ import NavBar from "../NavBar";
 
 function App() {
   return (
+    //in react-router-dom version 6 and above this is what works and there's no need of using the prop "exact"
     <div>
       <NavBar />
       <Routes>
-        <Route  path="/about" element={<About/>} />
+        <Route path="/about" element={<About/>} />
         <Route path="/login" element={ <Login />}/>
         <Route path="/" element={ <Home />}/>
       </Routes>
@@ -19,3 +20,33 @@ function App() {
 }
 
 export default App;
+
+// in react-router-dom version 5 the following code works
+
+// import React from "react";
+// import { Route, Switch } from "react-router-dom";
+// import Home from "./Home";
+// import About from "./About";
+// import Login from "./Login";
+// import NavBar from "./NavBar";
+
+// function App() {
+//   return (
+//     <div>
+//       <NavBar />
+//       <Switch>
+//         <Route exact path="/about">
+//           <About />
+//         </Route>
+//         <Route exact path="/login">
+//           <Login />
+//         </Route>
+//         <Route exact path="/">
+//           <Home />
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// }
+
+// export default App;
